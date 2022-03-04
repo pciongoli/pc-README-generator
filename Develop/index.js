b96c97cd913d14ddd1b2f5b62888602cf9cbe14b
pcion <patrick.ciongoli@gmail.com>
2022-03-04 03:14:30 -0500

    started adding sections to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return('inquirer');
+    prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please input the title of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What is the installation process for your project?',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the installation process for your project!')
+                    return false;
+                }
+            }
+        },
+        
+   
+    ])
+
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +44,5 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+init()
+    .then(questions)
